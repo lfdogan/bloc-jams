00cc223123d70683b356d1472f0427644ce59489
Lauren Dogan <laurenfdogan@gmail.com>
2015-11-30 15:29:30 -0500

    a22 complete dom scripting animation transitions on point

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,25 +1,29 @@
-var animatePoints = function() {/* will activate CSS transitions. it will update styles from CSS to this script. */
-     var points = document.getElementsByClassName('point');
-     var revealFirstPoint = function() {
-         points[0].style.opacity = 1; /* opacity changes from 0 (CSS) to 1 */
-         points[0].style.transform = "scaleX(1) translateY(0)"; /* scales from 90% (CSS) to 100% of width and translate to 3rem (CSS) up to normal position */
-         points[0].style.msTransform = "scaleX(1) translateY(0)";
-         points[0].style.WebkitTransform = "scaleX(1) translateY(0)";
-     };
-    var revealSecondPoint = function() {
-         points[1].style.opacity = 1;
-         points[1].style.transform = "scaleX(1) translateY(0)";
-         points[1].style.msTransform = "scaleX(1) translateY(0)";
-         points[1].style.WebkitTransform = "scaleX(1) translateY(0)";
-    };
-   var revealThirdPoint = function() {
-         points[2].style.opacity = 1;
-         points[2].style.transform = "scaleX(1) translateY(0)";
-         points[2].style.msTransform = "scaleX(1) translateY(0)";
-         points[2].style.WebkitTransform = "scaleX(1) translateY(0)";
-    };
-    revealFirstPoint();
-    revealSecondPoint();
-    revealThirdPoint();
+/* will activate CSS transitions. it will update styles from CSS to this script. */
+
+/* creates a Node List of all elements with "point" class */
+/* opacity changes from 0 (CSS) to 1 */
+/* TRANSFORM scales from 90% (CSS) to 100% of width and translate to 3rem (CSS) up to normal position */
+/* runs our script */
+
+
+
+var points = document.getElementsByClassName('point');
+
+var revealPoint = function(i) {
+    points[i].style.opacity = 1;
+    points[i].style.transform = "scaleX(1) translateY(0)";
+    points[i].style.msTransform = "scaleX(1) translateY(0)";
+    points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
 };
-/*animatePoints(); /* runs our script */
+
+var hidePoint = function(i) {
+    points[i].style.opacity = 0;
+    points[i].style.transform = "scaleX(1) translateY(0)";
+    points[i].style.msTransform = "scaleX(1) translateY(0)";
+    points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
+};
+
+for (var i = 0; i < points.length; i++) {
+    revealPoint(i);
+};
+
